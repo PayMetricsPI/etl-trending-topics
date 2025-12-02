8562beccd29aa03f2cb44791c4761e229f512bbb
guilhermesptech <guilherme.doliveira@sptech.school>
2025-12-02 01:06:22 -0300

    feat: serviço responsável por fornecer implementações para escrever em  arquivos CSV

--- src/main/java/infrastructure/csv/service/WriteCSVService.java
+++ src/main/java/infrastructure/csv/service/WriteCSVService.java
@@ -1,4 +1,26 @@
 package infrastructure.csv.service;
 
-public class ProcessorCSVService {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class WriteCSVService {
+    public void writeCSV(Path outputFile, List<String[]> lines) throws IOException {
+
+        String[] header = lines.get(0);
+
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(outputFile, StandardCharsets.UTF_8)) {
+            bufferedWriter.write(String.join(",", header));
+            bufferedWriter.newLine();
+
+            for (int i = 1; i < lines.size(); i++) {
+                String[] columns = lines.get(i);
+                bufferedWriter.write(String.join(",", columns));
+                bufferedWriter.newLine();
+            }
+        }
+    }
 }
