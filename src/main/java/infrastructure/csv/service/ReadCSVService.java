0ba494310587a4f67e674336fb699c79c849e4bf
guilhermesptech <guilherme.doliveira@sptech.school>
2025-12-02 01:05:51 -0300

    feat: serviço responsável por fornecer implementações para ler arquivos CSV

--- src/main/java/infrastructure/csv/service/ReadCSVService.java
+++ src/main/java/infrastructure/csv/service/ReadCSVService.java
@@ -1,4 +1,36 @@
 package infrastructure.csv.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadCSVService {
+
+    public List<Path> getCSVFiles(Path folderPath) throws IOException {
+        List<Path> csvFiles = new ArrayList<>();
+
+        try (DirectoryStream<Path> directory = Files.newDirectoryStream(folderPath, "*.csv")) {
+            for (Path path : directory) {
+                csvFiles.add(path);
+            }
+        }
+        return csvFiles;
+    }
+
+    public List<String[]> readCSV(Path filePath) throws IOException {
+        List<String[]> lines = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(filePath)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] columns = line.split(",");
+                lines.add(columns);
+            }
+        }
+        return lines;
+    }
 }
