4479bc599a6cb90ea60154ebffe8706907ff037e
guilhermesptech <guilherme.doliveira@sptech.school>
2025-12-02 01:04:32 -0300

    feat: serviço responsável por enviar arquivos para um bucket S3 da AWS

--- src/main/java/infrastructure/s3/service/S3SendService.java
+++ src/main/java/infrastructure/s3/service/S3SendService.java
@@ -1,4 +1,50 @@
-package infrastructure.s3.service;
+                                                                                                                                                                                                        package infrastructure.s3.service;
 
-public class S3SendService {
-}
+                                                                                                                                                                                                        import software.amazon.awssdk.services.s3.S3Client;
+                                                                                                                                                                                                        import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+                                                                                                                                                                                                        import java.nio.file.DirectoryStream;
+                                                                                                                                                                                                        import java.nio.file.Files;
+                                                                                                                                                                                                        import java.nio.file.Path;
+
+                                                                                                                                                                                                        public class S3SendService {
+                                                                                                                                                                                                            private final S3Client s3Client;
+                                                                                                                                                                                                            private final String bucketName;
+
+                                                                                                                                                                                                            public S3SendService(S3Client s3Client, String bucketName) {
+                                                                                                                                                                                                                this.s3Client = s3Client;
+                                                                                                                                                                                                                this.bucketName = bucketName;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               }
+
+                                                                                                                                                                                                            public void uploadFile(Path filePath) {
+                                                                                                                                                                                                                try {
+                                                                                                                                                                                                                    if (!Files.exists(filePath)) {
+                                                                                                                                                                                                                        System.out.println("Arquivo não encontrado");
+                                                                                                                                                                                                                        return;
+                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                    String fileName = filePath.getFileName().toString();
+
+                                                                                                                                                                                                                    PutObjectRequest objectRequest = PutObjectRequest.builder()
+                                                                                                                                                                                                                            .bucket(bucketName)
+                                                                                                                                                                                                                            .key("output/" + fileName)
+                                                                                                                                                                                                                            .contentType("text/csv; charset=UTF-8")
+                                                                                                                                                                                                                            .build();
+
+                                                                                                                                                                                                                    s3Client.putObject(objectRequest, filePath);
+                                                                                                                                                                                                                } catch (Exception e) {
+                                                                                                                                                                                                                    System.out.println(e.getMessage());
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                            public void uploadAllCSV(Path folderPath) {
+                                                                                                                                                                                                                try (DirectoryStream<Path> files = Files.newDirectoryStream(folderPath)) {
+                                                                                                                                                                                                                    for (Path filePath : files) {
+                                                                                                                                                                                                                        if (filePath.toString().endsWith(".csv")) {
+                                                                                                                                                                                                                            uploadFile(filePath);
+                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                } catch (Exception e) {
+                                                                                                                                                                                                                    System.out.println(e.getMessage());
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                            }
+                                                                                                                                                                                                        }
